41caef72ea717d93d81eba4d51aed85d7f6a19ed
shayzaran <shayzaran@gmail.com>
2021-11-21 15:46:06 +0300

    Initial Commit

---
+++ TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUICloneApp.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Selahattin Hayzaran on 21.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
