673a23ec55a561b7fa06d28e23473881bdca89c7
shayzaran <shayzaran@gmail.com>
2021-11-28 14:31:24 +0300

    tweet button added

---
+++ TwitterSwiftUIClone/View/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Selahattin Hayzaran on 21.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            TabView {
+                FeedView()
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                
+                Text("Search")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+                
+                Text("Message")
+                    .tabItem {
+                        Image(systemName: "envelope")
+                        Text("Messages")
+                    }
+            }
+            .navigationBarTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterSwiftUIClone/View/FeedView.swift
@@ -0,0 +1,36 @@
+//
+//  FeedView.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Selahattin Hayzaran on 28.11.2021.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+          
+            ScrollView {
+                
+            }
+            
+            Button(action: {}, label: {
+                Image(systemName: "plus")
+                    .resizable()
+                    .frame(width:32, height: 32)
+                    .padding()
+            })
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .padding()
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
